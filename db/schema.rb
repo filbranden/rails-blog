3dafc988b228ea91f14bf89856ad85f06a409444
Filipe Brandenburger <filbranden@gmail.com>
2020-07-19 18:06:38 -0700

    Add author to posts table
    $ bin/rails g migration add_author_to_posts author:string
    $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_235858) do
+ActiveRecord::Schema.define(version: 2020_07_20_003925) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
