5c8ed0b9d4fcf6553ae834bbc4d6cc2c14c63ba2
Filipe Brandenburger <filbranden@gmail.com>
2020-07-21 15:57:24 -0700

    Use a redirect for the root instead.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: :create
   end
-  root 'posts#index'
+  root to: redirect('/posts')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
